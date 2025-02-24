d8ce7ac078af2a0aed576fd2ae968c85018f50b5
ivanish2034 <ivanish.0119@gmail.com>
2025-02-24 15:18:25 +0300

    com 2
    Создал класс Heretic

---
+++ Maven1/src/main/java/mephi/b22901/maven1/Heretic.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mephi.b22901.maven1;
+
+/**
+ *
+ * @author ivis2
+ */
+public class Heretic {
+    private String name;
+    private String crimes;
+    private String lastSeen;
+    private String id;
+    private String threatLevel;
+
+    public Heretic(String name, String crimes, String lastSeen, String id, String threatLevel) {
+        this.name = name;
+        this.crimes = crimes;
+        this.lastSeen = lastSeen;
+        this.id = id;
+        this.threatLevel = threatLevel;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+    public String getCrimes() { 
+        return crimes; 
+    }
+    public String getLastSeen() { 
+        return lastSeen; 
+    }
+    public String getId() { 
+        return id; 
+    }
+    public String getLevel() { 
+        return threatLevel; 
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + threatLevel + ")";
+    }
+}
