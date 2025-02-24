ad6e40f7ded95b49d738f65eb31a4e344c5f95ae
ivanish2034 <ivanish.0119@gmail.com>
2025-02-24 14:05:06 +0300

    com1

---
+++ Maven1/src/main/java/mephi/b22901/maven1/Maven1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.maven1;
+
+/**
+ *
+ * @author ivis2
+ */
+public class Maven1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
